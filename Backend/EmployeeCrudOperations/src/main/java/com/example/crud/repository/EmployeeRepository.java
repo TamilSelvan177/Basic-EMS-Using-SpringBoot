c71fcfede6567ad9a0a5bd46a6d2139b501cb12a
Tamilselvan <tamilselvanaug05@gmail.com>
2026-05-25 14:16:04 +0530

    Removed frontend folder

--- Backend/EmployeeCrudOperations/src/main/java/com/example/crud/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.crud.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
--- Backend/EmployeeCrudOperations/src/main/java/com/example/crud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.crud.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
